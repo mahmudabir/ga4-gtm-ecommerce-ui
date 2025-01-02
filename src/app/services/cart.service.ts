a9210bc44999f196f42aeb95977084245c7647da
Abir Mahmud <amabirmahmud@gmail.com>
2025-01-03 03:56:58 +0600

    add to cart - push in dataLayer

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {DataLayerService} from './data-layer.service';
 
 @Injectable({
   providedIn: 'root',
@@ -6,10 +7,20 @@ import {Injectable} from '@angular/core';
 export class CartService {
   private cart: any[] = [];
 
-  constructor() {}
+  constructor(private dataLayerService: DataLayerService) {}
 
   addToCart(product: any): void {
     this.cart.push(product);
+
+    this.dataLayerService.push({
+      event: "product_added_to_cart",
+      page_url: `/products`,
+      product_page_url: `/products/${product.id}`,
+      product_id: `${product.id}`,
+      product_name: product.name,
+      product_price: product.price,
+      product: product,
+    });
   }
 
   getCart(): any[] {
